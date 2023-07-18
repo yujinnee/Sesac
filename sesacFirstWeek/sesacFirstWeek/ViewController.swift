499e4ead835563ebaaa3419124d33da31f835de6
Yujin Lee <yj587878@gmail.com>
2023-07-18 16:25:35 +0900

    [FEAT] 7/14 강의 예제

---
+++ sesacFirstWeek/sesacFirstWeek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sesacFirstWeek
+//
+//  Created by 이유진 on 2023/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
