e323afa369cc9c149b66999de9c2af869161d3da
Yujin Lee <yj587878@gmail.com>
2023-07-23 02:26:07 +0900

    [FEAT] 신조어 프로젝트 파일 및 에셋 추가

---
+++ NewlyCoinedWordProject/NewlyCoinedWordProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewlyCoinedWordProject
+//
+//  Created by 이유진 on 2023/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
