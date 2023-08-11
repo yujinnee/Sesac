b93dc9fbe4404ea5bfd3553e19b207cb29775611
Yujin Lee <yj587878@gmail.com>
2023-08-11 23:20:25 +0900

    [FIX] 지워버린 과제 코드 주석으로 추가

--- SeSAC3Week4/SeSAC3Week4/ViewControllers/TranslationViewController.swift
+++ SeSAC3Week4/SeSAC3Week4/ViewControllers/TranslationViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class TranslationViewController: UIViewController {
     
     
+    
+    
     @IBOutlet var originalTextView: UITextView!
     @IBOutlet var requestButton: UIButton!
     @IBOutlet var translateTextView: UITextView!
@@ -21,30 +23,14 @@ class TranslationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
- 
-        
-        UserDefaults.standard.set("고래밥", forKey: "nickname")
-        UserDefaults.standard.set(33,forKey:"age")
-        UserDefaultsHelper.standard.nickname = "칙촉"
-        UserDefaultsHelper.standard.age = 23
-        
-        
-//        originalTextView.text = UserDefaults.standard.string(forKey: "nickname")
-//        originalTextView.text = "\(UserDefaults.standard.integer(forKey: "age"))"
-//        originalTextView.text = helper.standard.nickname
-//        originalTextView.text = "\(helper.standard.age)"
-        
-        
+
         UserDefaultsHelper.standard.nickname = "칙촉"
         UserDefaultsHelper.standard.age = 23
         originalTextView.text = UserDefaultsHelper.standard.nickname
         originalTextView.text = "\(UserDefaultsHelper.standard.age)"
         
 
-        
-        
-        
+
         
         originalTextView.text = ""
         translateTextView.text = ""
@@ -56,6 +42,62 @@ class TranslationViewController: UIViewController {
         TranslateAPIManager.shared.callRequest(text: originalTextView.text ?? "") { result in
             self.translateTextView.text = result
         }
+        /*
+        
+        let detectionUrl = "https://openapi.naver.com/v1/papago/detectLangs"
+               
+               let translationUrl = "https://openapi.naver.com/v1/papago/n2mt"
+               let header: HTTPHeaders = [
+                   "X-Naver-Client-Id": APIKey.naverClientId,
+                   "X-Naver-Client-Secret": APIKey.naverClientSecret
+               ]
+               
+               let detectionParameter: Parameters = [
+                   "query": originalTextView.text ?? ""
+               ]
+               
+               AF.request(detectionUrl,method:.post,parameters:detectionParameter,headers: header).validate().responseJSON{ response in
+                   switch response.result {
+                   case .success(let value):
+                       let json = JSON(value)
+                       print("JSON:\(json)")
+                       
+                       let langCode = json["langCode"].stringValue
+
+                       let translationParameters: Parameters = [
+                           "source": "\(langCode)",
+                           "target": "en",
+                           "text": self.originalTextView.text ?? ""
+                       ]
+                       
+                       AF.request(translationUrl, method: .post,parameters: translationParameters ,headers: header).validate().responseJSON { response in
+                           switch response.result {
+                           case .success(let value):
+                               let json = JSON(value)
+                               print("JSON: \(json)")
+                               
+                               let data = json["message"]["result"]["translatedText"].stringValue
+                               self.translateTextView.text = data
+                               
+                           case .failure(let error):
+                               print("번역 오류")
+                               print(error)
+                           }
+                       }
+
+                       
+                   case .failure(let error):
+                       print("언어감지 오류")
+                       print(error)
+                   }
+                   
+                   
+               }
+               
+              
+           }
+         */
+           
     }
     
     
