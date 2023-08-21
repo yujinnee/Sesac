310dc127b167f2fcfc426b6622bda746ea2948d4
Yujin Lee <yj587878@gmail.com>
2023-08-22 01:41:30 +0900

    [FEAT] MediaProject More Page 구현

---
+++ MediaProject/MediaProject/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  MediaProject
+//
+//  Created by 이유진 on 2023/08/12.
+//
+
+import Foundation
+struct APIKeys{
+    static let tmdbKey = "d3e8c335b7a2d97547affb46f5428efc"
+}
+
+
